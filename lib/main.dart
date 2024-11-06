b4bd45c5ece3e2809fc0e29cfd7e0c7f45eca7f8
mhardiansyah <mhardiansyah5766@gmail.com>
2024-11-06 10:33:15 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
     );
   }
 }
