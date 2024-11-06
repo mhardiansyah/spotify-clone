f4a28a964dd32652a9392e4dec23cdf5a4c30804
mhardiansyah <mhardiansyah5766@gmail.com>
2024-11-06 11:16:53 +0700

    four commit

--- lib/Themes/colors.dart
+++ lib/Themes/colors.dart
@@ -3,3 +3,6 @@ import 'package:flutter/material.dart';
 Color primaryColor = Color(0xFF121212);
 
 Color second = Color(0xFF1ED760);
+
+
+
---
+++ lib/UI/page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:spotify/Themes/colors.dart';
+import 'package:spotify/UI/Home.dart';
+import 'package:spotify/UI/Library.dart';
+import 'package:spotify/UI/Premium.dart';
+import 'package:spotify/UI/Search.dart';
+
+class page extends StatefulWidget {
+  const page({super.key});
+
+  @override
+  State<page> createState() => _pagetState();
+}
+
+class _pagetState extends State<page> {
+  int _currentIndex = 0;
+  final List<Widget> _page = [
+    Center(
+      child: Home(),
+    ),
+    Center(
+      child: Search(),
+    ),
+    Center(
+      child: Library(),
+    ),
+    Center(
+      child: Premium(),
+    ),
+  ];
+  void _onTapTapp(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: _page[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: second,
+        unselectedItemColor: Color(0xff6C6C6C),
+        currentIndex: _currentIndex,
+        onTap: _onTapTapp,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Beranda'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.library_books), label: ' Your Library'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.workspace_premium_outlined), label: 'Premium'),
+        ],
+      ),
+    );
+  }
+}
